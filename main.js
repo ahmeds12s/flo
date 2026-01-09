dc9e62019dd0c2e97ef77dd1e04bbd865228808f
ahmeds12s <74270685+ahmeds12s@users.noreply.github.com>
2026-01-09 13:49:18 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+    const sound = new Audio("click.mp3");
+
+    document.body.addEventListener("click", (e) => {
+    const link = e.target.closest("a");
+
+    if (!link) return;
+
+    e.preventDefault();
+
+    sound.currentTime = 0;
+    sound.play().catch(() => {});
+
+
+    setTimeout(() => {
+        window.location.href = link.href;
+    }, 150); 
+    });
+});
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const swiper = new Swiper(".js-testimonials-slider", {
+    grabCursor: true,
+    spaceBetween: 30,
+
+    pagination: {
+      el: ".js-testimonials-pagination",
+      clickable: true
+    },
+
+    slidesPerView: 1,
+    breakpoints: {
+      767: {
+        slidesPerView: 3
+      }
+    }
+  });
+});
